6df63caa047cd66f09009fe0c2339c2eab8244af
Aurelie Coudert <aurelie.proffit@gmail.com>
2022-11-28 15:57:58 +0100

    user model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Aurélie : had the relation to the pets table -> One user can have many pets
+  has_many :pets
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
