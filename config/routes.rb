28f483e30d4fb1744aab70401f9f6afa86972daf
Lucia Fontana <lucia.fontana1987@gmail.com>
2022-11-28 15:46:44 +0100

    Pets MVC

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [ :index, :show, :new, :create ] do
+    resources :pets, only: [ :index, :show, :new, :create ]
+  end
 end
