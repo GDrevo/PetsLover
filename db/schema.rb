92ce4ed53a831994baec3e25f989073ebf2c19a5
Aurelie Coudert <aurelie.proffit@gmail.com>
2022-11-28 16:58:43 +0100

    updated user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_140127) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_142720) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string "specie"
+    t.string "breed"
+    t.text "description"
+    t.integer "age"
+    t.float "price"
+    t.string "address"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_140127) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pets", "users"
 end
