bb6f701e0bb7d91291a4464ee99e0135751fd565
Lucia Fontana <lucia.fontana1987@gmail.com>
2022-11-29 15:34:00 +0100

    Form with image

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -24,6 +24,6 @@ class PetsController < ApplicationController
   private
 
   def pet_params
-    params.require(:pet).permit(:specie, :breed, :description, :age, :price, :address)
+    params.require(:pet).permit(:specie, :breed, :description, :age, :price, :address, :photo)
   end
 end
