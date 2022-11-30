f334373814ca834d126ea9dee986748954c689ff
Aurelie Coudert <aurelie.proffit@gmail.com>
2022-11-30 10:11:35 +0100

    work in progress

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,8 @@
 class Pet < ApplicationRecord
   belongs_to :user
   validates :specie, presence: true
-  has_one_attached :file
+  has_one_attached :photo
+
+  geocoded_by :address
+  after_validation :geocode, if :will_save_change_to_address?
 end
