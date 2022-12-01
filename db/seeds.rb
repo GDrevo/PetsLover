0e302a752782279ac770a287ea55c775dadaa469
Aurelie Coudert <aurelie.proffit@gmail.com>
2022-12-01 16:07:30 +0100

    seeds now ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
+Purchase.destroy_all
+Pet.destroy_all
+User.destroy_all
 
 # pets model columns : title, specie, description, gender, age(integer), price(integer), address
 
